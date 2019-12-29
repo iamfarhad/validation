097911c43da4b7ee8eaeb1330856168355625021
farhad zand <farhad.pd@gmail.com>
2019-12-29 17:34:53 +0330

    made some change in sheba rule
    Signed-off-by: farhad zand <farhad.pd@gmail.com>

--- src/Rules/ShebaNumber.php
+++ src/Rules/ShebaNumber.php
@@ -15,28 +15,29 @@ class ShebaNumber extends AbstractValidationRule implements ValidationRuleInterf
     public function rule($attribute, $value, $parameters, $validator): bool
     {
         $ibanReplaceValues = [];
-        if (!empty($value)) {
-            $value = preg_replace('/[\W_]+/', '', strtoupper($value));
-            if ((4 > strlen($value) || strlen($value) > 34) || (is_numeric($value [0]) || is_numeric($value [1])) || (!is_numeric($value [2]) || !is_numeric($value [3]))) {
-                return false;
-            }
-            $ibanReplaceChars = range('A', 'Z');
-            foreach (range(10, 35) as $tempvalue) {
-                $ibanReplaceValues[] = strval($tempvalue);
-            }
-            $tmpIBAN = substr($value, 4) . substr($value, 0, 4);
-            $tmpIBAN = str_replace($ibanReplaceChars, $ibanReplaceValues, $tmpIBAN);
-            $tmpValue = intval(substr($tmpIBAN, 0, 1));
-            for ($i = 1; $i < strlen($tmpIBAN); $i++) {
-                $tmpValue *= 10;
-                $tmpValue += intval(substr($tmpIBAN, $i, 1));
-                $tmpValue %= 97;
-            }
-            if ($tmpValue != 1) {
-                return false;
-            }
-            return true;
+
+        if (empty($value))
+            return false;
+
+        $value = preg_replace('/[\W_]+/', '', strtoupper($value));
+        if ((4 > strlen($value) || strlen($value) > 34) || (is_numeric($value [0]) || is_numeric($value [1])) || (!is_numeric($value [2]) || !is_numeric($value [3]))) {
+            return false;
         }
-        return false;
+        $ibanReplaceChars = range('A', 'Z');
+        foreach (range(10, 35) as $tempvalue) {
+            $ibanReplaceValues[] = strval($tempvalue);
+        }
+        $tmpIBAN = substr($value, 4) . substr($value, 0, 4);
+        $tmpIBAN = str_replace($ibanReplaceChars, $ibanReplaceValues, $tmpIBAN);
+        $tmpValue = intval(substr($tmpIBAN, 0, 1));
+        for ($i = 1; $i < strlen($tmpIBAN); $i++) {
+            $tmpValue *= 10;
+            $tmpValue += intval(substr($tmpIBAN, $i, 1));
+            $tmpValue %= 97;
+        }
+        if ($tmpValue != 1) {
+            return false;
+        }
+        return true;
     }
 }
