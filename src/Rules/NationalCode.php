7942882b65d537f856b7eb7b601d13315a986cda
iamfarhad <iamfarhad@users.noreply.github.com>
2021-12-28 20:41:01 +0000

    Fix styling

--- src/Rules/NationalCode.php
+++ src/Rules/NationalCode.php
@@ -15,7 +15,7 @@ class NationalCode implements Rule
     {
         $this->attribute = $attribute;
 
-        if (!preg_match('/^\d{8,10}$/', $value) || preg_match('/^[0]{10}|[1]{10}|[2]{10}|[3]{10}|[4]{10}|[5]{10}|[6]{10}|[7]{10}|[8]{10}|[9]{10}$/', $value)) {
+        if (! preg_match('/^\d{8,10}$/', $value) || preg_match('/^[0]{10}|[1]{10}|[2]{10}|[3]{10}|[4]{10}|[5]{10}|[6]{10}|[7]{10}|[8]{10}|[9]{10}$/', $value)) {
             return false;
         }
         $sub = 0;
